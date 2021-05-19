9e89b28f83b3ba4e3b043568dfa04f6f81656ba3
Abdurasul04 <80384632+Abdurasul04@users.noreply.github.com>
2021-05-20 00:19:51 +0500

    Delete movie.php

--- movie.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo '<h1 style="text-align: center ; background-color: cornflowerblue"><a href=' . 'https://rezka.ag/search/?do=search&subaction=search&q='.$_GET['search'] . '>Нажми сюда</a></h1>';
