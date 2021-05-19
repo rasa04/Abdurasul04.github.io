d42a4ee83bb1f41a5de8d273bb19850719587ae7
Abdurasul04 <80384632+Abdurasul04@users.noreply.github.com>
2021-05-20 00:16:44 +0500

    Add files via upload

---
+++ recognize.php
@@ -0,0 +1,2 @@
+<?php
+echo '<h1 style="text-align: center ; background-color: cornflowerblue"><a href=' . 'https://rezka.ag/search/?do=search&subaction=search&q='.$_GET['search'] . '>Нажми сюда</a></h1>';
